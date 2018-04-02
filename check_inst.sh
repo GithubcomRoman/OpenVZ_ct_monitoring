97915564a325c18a0b7b96b12abaa65336e0037a
GithubcomRoman <35697476+GithubcomRoman@users.noreply.github.com>
2018-04-02 14:28:28 +0200

    install.sh
    install.sh done

--- check_inst.sh
+++ check_inst.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
-echo "Сheck Zabbix Agent activ..."
+echo "|--Check Zabbix Agent activ..."
 
 
-[ "$(pgrep zabbix)" ] && echo "|----- OK - Zabbix agent run" || echo "|----- FAIL - Zabbix agent run"
+[ "$(pgrep zabbix)" ] && echo "+OK - Zabbix agent run" || echo "-FAIL - Zabbix agent run"
 
 FILE[1]="/etc/zabbix/zabbix_agentd.d/open_vz.conf"
 FILE[2]="/etc/zabbix/scripts/ct_param_name.sh"
 FILE[3]="/etc/zabbix/scripts/ct_param_value.sh"
 FILE[4]="/etc/zabbix/scripts/ct_failcnt_name.sh"
 FILE[5]="/etc/zabbix/scripts/ct_failcnt_value.sh"
+FILE[6]="/root/ct_check_v02.sh"
+FILE[7]="/root/failcnt_count.sh"
 
-echo "Сheck openZV config for Zabbix Agent..."
+echo "|--Check openZV config for Zabbix Agent..."
 if [ ! -f "${FILE[1]}" ]; then
-    echo "|----- FAIL - /etc/zabbix/zabbix_agentd.d/open_vz.conf"
-        exit 1;
+    echo "-FAIL - /etc/zabbix/zabbix_agentd.d/open_vz.conf"
+       # exit 1;
         
         else 
-        echo "|----- OK - /etc/zabbix/zabbix_agentd.d/open_vz.conf"
+        echo "+OK - /etc/zabbix/zabbix_agentd.d/open_vz.conf"
 fi
 
 
-echo "Сheck scripts for Zabbix Agent..."
+echo "|--Check scripts for Zabbix Agent..."
 
 for ((count=2; count <= ${#FILE[*]}; count++))
 do
 if [ ! -f "${FILE[$count]}" ]; then
-    echo "|----- FAIL - ${FILE[$count]} - not found!"
-        exit 1;
+    echo "-FAIL - ${FILE[$count]} - not found!"
+       # exit 1;
         
         else 
-        echo "|----- OK - ${FILE[$count]}"
+        echo "+OK - ${FILE[$count]}"
 fi
 
 done
--- install.sh
+++ install.sh
@@ -16,4 +16,43 @@ chmod -R +x /etc/zabbix/scripts
 
 
 cp ct_check_v02.sh /root/ct_check_v02.sh && chmod -R +x /root/ct_check_v02.sh
-cp ailcnt_count.sh /root/failcnt_count.sh && chmod -R +x /root/failcnt_count.sh
+cp failcnt_count.sh /root/failcnt_count.sh && chmod -R +x /root/failcnt_count.sh
+
+#-----------------| Check install. Checking the availability of files. |--------------------|
+
+echo "|--Check Zabbix Agent activ..."
+
+
+[ "$(pgrep zabbix)" ] && echo "+OK - Zabbix agent run" || echo "-FAIL - Zabbix agent run"
+
+FILE[1]="/etc/zabbix/zabbix_agentd.d/open_vz.conf"
+FILE[2]="/etc/zabbix/scripts/ct_param_name.sh"
+FILE[3]="/etc/zabbix/scripts/ct_param_value.sh"
+FILE[4]="/etc/zabbix/scripts/ct_failcnt_name.sh"
+FILE[5]="/etc/zabbix/scripts/ct_failcnt_value.sh"
+FILE[6]="/root/ct_check_v02.sh"
+FILE[7]="/root/failcnt_count.sh"
+
+echo "|--Check openZV config for Zabbix Agent..."
+if [ ! -f "${FILE[1]}" ]; then
+    echo "-FAIL - /etc/zabbix/zabbix_agentd.d/open_vz.conf"
+       # exit 1;
+        
+        else 
+        echo "+OK - /etc/zabbix/zabbix_agentd.d/open_vz.conf"
+fi
+
+
+echo "|--Check scripts for Zabbix Agent..."
+
+for ((count=2; count <= ${#FILE[*]}; count++))
+do
+if [ ! -f "${FILE[$count]}" ]; then
+    echo "-FAIL - ${FILE[$count]} - not found!"
+       # exit 1;
+        
+        else 
+        echo "+OK - ${FILE[$count]}"
+fi
+
+done
